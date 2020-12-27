c53b8dc104e7f96e1ceca1cca63b3a3b962bedf6
MOE-Shimada <64189888+MOE-Shimada@users.noreply.github.com>
2020-12-27 22:21:12 +0900

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -91,7 +91,7 @@ function AddItemForm({ onNewItem }) {
 
     return (
         <Form onSubmit={submitNewItem}>
-            <h1>Todo App</h1>
+            <h2>Todo App</h2>
             <InputGroup className="mb-3">
                 <Form.Control
                     value={newItem}
